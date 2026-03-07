5f0c28fed527e095420b3192e230f34e474ffea7
Swarn Android <swarnadityasingh@proton.me>
2026-03-07 22:28:22 +0530

    added vim

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 printf "%s\n" "setting up termux for you"
 
-pkg in termux-api termux-services fastfetch stow openssh zsh fzf starship figlet eza zoxide bat -y > /dev/null
+pkg in termux-api termux-services vim fastfetch stow openssh zsh fzf starship figlet eza zoxide bat -y > /dev/null
 
 sv-enable sshd
 sv start sshd
