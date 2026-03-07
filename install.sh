4f4b0bc588497093e45a2184d9cf1cef2b86786f
Swarn Android <swarnadityasingh@proton.me>
2026-03-07 21:03:18 +0530

    fixed some stuff

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-cat << EOF
-          _               
- ___  ___| |_ _   _ _ __  
-/ __|/ _ \ __| | | | '_ \ 
-\__ \  __/ |_| |_| | |_) |
-|___/\___|\__|\__,_| .__/ 
-                   |_|    
+printf "%s\n" "setting up termux for you"
 
-EOF
+pkg in termux-api fastfetch stow zsh fzf starship figlet eza zoxide bat -y > /dev/null
 
-pkg in termux-api stow zsh fzf starship figlet eza zoxide bat -y
+printf "%s\n" "applying dotfiles"
+
 cd ~/.dots
 stow --ignore=.assets --ignore=readme.md --ignore=install.sh .
+
+printf "%s\n" "setting up zsh as default shell"
+
 chsh -s zsh
+
+printf "%s\n" "setting up custom motd"
+
 figlet "fsociety" > $PREFIX/etc/motd
+
+printf "%s\n" "please restart termux"
